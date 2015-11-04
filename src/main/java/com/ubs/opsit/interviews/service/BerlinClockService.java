ea4295f38dfe27df3d9cce712b51d65999b44fed
vinod <vinodsubhash.mvt@gmail.com>
2015-11-04 06:34:59 +0100

    Update the service implementation comments

--- src/main/java/com/ubs/opsit/interviews/service/BerlinClockService.java
+++ src/main/java/com/ubs/opsit/interviews/service/BerlinClockService.java
@@ -6,11 +6,18 @@ import com.ubs.opsit.interviews.domain.BerlinClock;
  * Service call to implement TimeConverter interface and call berlinClock method for representation
  */
 public class BerlinClockService implements TimeConverter {
+
     BerlinClock berlinClock;
+
     public BerlinClockService(){
         berlinClock = new BerlinClock();
     }
 
+    /**
+     * Service delegator to convert time to clock representation.
+     * @param strTime
+     * @return str
+     */
     @Override
     public String convertTime(final String strTime) {
        return berlinClock.convertTime(strTime);
